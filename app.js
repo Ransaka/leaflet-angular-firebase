d812fc0cc89b14b7eb5c7394ebf8790d71743f19
Gonzalo Silverio <gsilver@umich.edu>
2016-04-14 12:17:19 -0400

    search on category or content filters, start with different icons for different categories

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var app = angular.module( "demoapp", [ 'ui-leaflet' ] );
+var app = angular.module( "bofapp", [ 'ui-leaflet' ] );
 var bofDataRef = new Firebase( 'https://kitb1w34vt8.firebaseio-demo.com/bofs' );
-app.controller( 'MarkersEventsAddController', [ '$scope', '$filter', '$timeout', '$log', 'leafletData', function ( $scope, $filter, $timeout, $log, leafletData ) {
+app.controller( 'mapController', [ '$scope', '$filter', '$timeout', '$log', 'leafletData', function ( $scope, $filter, $timeout, $log, leafletData ) {
   angular.extend( $scope, {
     center: {
-      zoom:16,
+      zoom: 16,
       autoDiscover: true
     },
     events: {},
@@ -39,45 +39,60 @@ app.controller( 'MarkersEventsAddController', [ '$scope', '$filter', '$timeout',
     }
   } );
 
-  $scope.bofIt = function() {
-    leafletData.getMap().then(function(map) {
-      map.locate({setView: true, maxZoom: 16});
-      function onLocationFound(e) {
-          var radius = e.accuracy / 2;
-          $( '#bofModal' ).attr( 'data-coords', [ e.latlng.lat, e.latlng.lng ] );
-          L.marker(e.latlng).addTo(map)
-              .bindPopup('<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>').openPopup();
-          L.circle(e.latlng, radius).addTo(map);
+  $scope.categories = null;
+
+  $scope.bofIt = function () {
+    leafletData.getMap().then( function ( map ) {
+      map.locate( {
+        setView: true,
+        maxZoom: 16
+      } );
+
+      function onLocationFound( e ) {
+        var radius = e.accuracy / 2;
+        $( '#bofModal' ).attr( 'data-coords', [ e.latlng.lat, e.latlng.lng ] );
+        L.popup()
+          .setLatLng( e.latlng )
+          .setContent( '<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>' )
+          .openOn( map );
       }
-      map.on('locationfound', onLocationFound);
-    });
+      map.on( 'locationfound', onLocationFound );
+    } );
   };
 
   $scope.markers = [];
+  $scope.markersAll = [];
 
   bofDataRef.on( 'child_added', function ( snapshot ) {
     var marker = snapshot.val();
-    $scope.markers.push( {
+    newMarker = {
       lat: parseFloat( marker.lat ),
       lng: parseFloat( marker.long ),
-      message: marker.what,
-      layer: 'bofs'//,
-      //draggable:marker.draggable
-    } );
+      category: marker.category,
+      message: '<p>' + marker.what + '</p><p>' + marker.category + '</p>',
+      layer: 'bofs'
+    };
+    $scope.markersAll.push(newMarker);
+    $scope.markers.push(newMarker);
   } );
 
   $scope.$on( "leafletDirectiveMap.click", function ( event, args ) {
-    var leafEvent = args.leafletEvent;
-    $( '#bofModal' ).attr( 'data-coords', [ leafEvent.latlng.lat, leafEvent.latlng.lng ] );
-    $( '#bofModal' ).modal();
+    leafletData.getMap().then( function ( map ) {
+      var leafEvent = args.leafletEvent;
+      $( '#bofModal' ).attr( 'data-coords', [ leafEvent.latlng.lat, leafEvent.latlng.lng ] );
+      L.popup()
+        .setLatLng( leafEvent.latlng )
+        .setContent( '<a href="" data-toggle="modal" data-target="#bofModal">Add an event here?</a>' )
+        .openOn( map );
+    } );
   } );
-
 
   $( '#postBof' ).on( 'click', function () {
     var what = $( '#messageText' ).val();
     var start = $( '#newStartTime' ).val();
     var end = $( '#newEndTime' ).val();
     var coords = $( '#bofModal' ).attr( 'data-coords' ).split( ',' );
+
     var marker = {
       user: 'user',
       what: what,
@@ -87,15 +102,35 @@ app.controller( 'MarkersEventsAddController', [ '$scope', '$filter', '$timeout',
       long: coords[ 1 ],
       maybe: 0,
       sure: 0,
-      draggable:true
+      category: $scope.categories,
+      draggable: true
     };
     bofDataRef.push( marker );
+    leafletData.getMap().then( function ( map ) {
+      map.closePopup();
+    } );
     $( '#messageText, #newStartTime, #newEndTime' ).val( '' );
   } );
 
-  $scope.$watch("markers", function() {
-    $scope.$watch('markerFilter', function (text) {
-      $scope.markersFiltered = $filter('filter')($scope.markers, {message:text});
-    });
-  },true);
+
+  $( '#categories a' ).on( 'click', function ( e ) {
+    var category = $( this ).attr( 'id' );
+    if ( category === 'all' ) {
+      $scope.markers = $scope.markersAll;
+    } else {
+      $scope.markers = $scope.markersAll;
+      $scope.markers = $filter( 'filter' )( $scope.markers, {
+        category: category
+      } );
+    }
+  } );
+
+
+  $scope.$watch( "markers", function () {
+    $scope.$watch( 'markerFilter', function ( text ) {
+      $scope.markersFiltered = $filter( 'filter' )( $scope.markers, {
+        message: text
+      } );
+    } );
+  }, true );
 } ] );
